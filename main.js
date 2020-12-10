ba938a4e500f33ba511861c426272ec04e5faee2
Tam O'Donnell <thomashodonnell@gmail.com>
2020-12-10 22:17:56 +0000

    add basic information and console related libs

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
-const { username, osCode, osVersion } = require('./lib/os');
+//console helper libraries
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
 
-const greeting = `Hello ${username}, welcome to the System Reporter CLI. Your current system is ${osCode}, version ${osVersion}.`;
+//system info imports
+const { username, osName, osRelease } = require('./lib/os');
+const { freeMemory, totalMemory, memoryUsage } = require('./lib/memory');
+const { numOfCpus, averageSpeedInGHz } = require('./lib/processors');
+const { homeDirectory, uptimeInHours } = require('./lib/system');
+
+const banner = chalk.green(
+  figlet.textSync('System Reporter', { horizontalLayout: 'full' })
+);
+
+clear();
+
+console.log(banner);
+
+const greeting = `Hello ${username}, welcome to the System Reporter CLI. Your current system is ${osName}, version ${osRelease}.`;
 
 console.log(greeting);
+
+const memoryInformation = `You have a total of ${totalMemory} bytes of memory, ${freeMemory} of which are free - that's ${memoryUsage.toFixed(
+  2
+)}%!`;
+
+console.log(memoryInformation);
+
+const cpuInformation = `You have ${numOfCpus} processors or cores runnning at an average of ${averageSpeedInGHz}GHz.`;
+
+console.log(cpuInformation);
+
+const otherSystemInfo = `Your home directory is ${homeDirectory} and your system has been live for ${uptimeInHours} hours.`;
+
+console.log(otherSystemInfo);
